bedc2334251c20bc4cfa7db6cb970e210e9d19d2
Alexis 'Horgix' Chotard <alexis.horgix.chotard@gmail.com>
2016-02-04 16:17:00 +0100

    Plugin: remove python3 things, clean, comment

--- weechat-french-to-sms.py
+++ weechat-french-to-sms.py
@@ -3,7 +3,6 @@
 import weechat
 import urllib
 import urllib2
-
 import re
 import HTMLParser
 
@@ -11,29 +10,23 @@ weechat.register("sms", "horgix", "0.1", "Beerware",
         "French to SMS via http://raphlight.free.fr/trad", "", "")
 
 def sms_cb(data, buffer, args):
-    #nick = weechat.buffer_get_string(weechat.current_buffer(), 'localvar_nick')
-
+    # Basics parameters
     url = 'http://raphlight.free.fr/trad'
-    values = {'convertir': 'Convertir'}
-    values['message'] = args
+    values = {'message': args, 'convertir': 'Convertir'}
 
-    #params = urllib.parse.urlencode(values).encode('utf-8')
-    params = urllib.urlencode(values)#
-    #req = urllib.request.Request(url, params)
+    # Execute request
+    params = urllib.urlencode(values)
     req = urllib2.Request(url, params)
     the_page = urllib2.urlopen(req).read()
-    #req.add_header("Content-Type","application/x-www-form-urlencoded;charset=utf-8")
-    #with urllib.request.urlopen(req) as response:
-    #    the_page = response.read().decode('utf-8')
+    # Parse answer page (why not an API that answers in json :( )
     result = [ e for e in the_page.splitlines() if 'result' in e ][0]
     result = re.sub('.*id=\'result\'> *', '', result)
     result = re.sub(' *</div>.*', '', result)
-    h = HTMLParser.HTMLParser()
-    result = h.unescape(result.decode('utf-8'))
-    args = result.encode('utf-8')
-    weechat.command("", args)
+    # Decode, unescape, reencode
+    result = HTMLParser.HTMLParser().unescape(result.decode('utf-8'))
+    answer = result.encode('utf-8')
+    # Send answer to weechat
+    weechat.command("", answer)
     return weechat.WEECHAT_RC_OK
 
 hook = weechat.hook_command("sms", "French to SMS", "text", "", "", "sms_cb", "")
-
-#sms_cb(None, None, "Bonjour ceci est un test")
